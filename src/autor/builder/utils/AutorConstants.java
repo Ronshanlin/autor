4fb5ede3d889556259a01adcccd45baa3942ed73
Ronshanlin <ronshanlin@foxmail.com>
2015-03-17 20:23:32 +0800

    修改序列化文件路径
    2015-03-17 20:23

--- src/autor/builder/utils/AutorConstants.java
+++ src/autor/builder/utils/AutorConstants.java
@@ -8,46 +8,47 @@ package autor.builder.utils;
  *
  */
 public class AutorConstants {
-	/**
-	 * 自定义builder Id
-	 */
-	public static final String BUILDER_ID = "autor.autorBuilder";
-	/**
-	 * 自定义Marker Id
-	 */
-	public static final String MARKER_TYPE = "autor.xmlProblem";
-	/**
-	 * xml 文件名前缀
-	 */
-	public static final String SQLMAP_PREFIX = "sqlmap";
-	/**
-	 * 自定义资源路径名
-	 */
-	public static final String SRC_FOLDER_GEN = "gen";
-	/**
-	 * maven nature
-	 */
-	public static final String MVN_NATURE = "org.eclipse.m2e.core.maven2Nature";
-	/**
+    /**
+     * 自定义builder Id
+     */
+    public static final String BUILDER_ID = "autor.autorBuilder";
+    /**
+     * 自定义Marker Id
+     */
+    public static final String MARKER_TYPE = "autor.xmlProblem";
+    /**
+     * xml 文件名前缀
+     */
+    public static final String SQLMAP_PREFIX = "sqlmap";
+    /**
+     * 自定义资源路径名
+     */
+    public static final String SRC_FOLDER_GEN = "gen";
+    /**
+     * maven nature
+     */
+    public static final String MVN_NATURE = "org.eclipse.m2e.core.maven2Nature";
+    /**
      * maven项目编译文件夹
      */
     public static final String MVN_COMPILE_FOLDER_TARGET = "target";
     public static final String MVN_SRC_MAIN_JAVA = "src/main/java";
-	
-	/**
+    
+    /**
      * java文件名
      */
     public static final String FILE_R_NAME = "R.java";
-	public static final String FILE_PATH = System.getProperty("user.dir");
-	public static final String FILE_SUFFIX_JSON = ".json";
-	public static final String FILE_ENCODE = "utf-8";
-	/**
-	 * 分隔符
-	 */
-	public static final String SEPARATOR_SLASH="\\";
-	public static final String SEPARATOR_BACKSLASH="/";
-	public static final String SEPARATOR_DOT=".";
-	public static final String SEPARATOR_LINE="_|-";
+    public static final String FILE_PATH = System.getProperty("user.dir");
+    public static final String FILE_SUFFIX_JSON = ".json";
+    public static final String FILE_ENCODE = "utf-8";
+    public static final String FILE_DIR_SETTINGS=".settings";
+    /**
+     * 分隔符
+     */
+    public static final String SEPARATOR_SLASH="\\";
+    public static final String SEPARATOR_BACKSLASH="/";
+    public static final String SEPARATOR_DOT=".";
+    public static final String SEPARATOR_LINE="_|-";
     /**
      * 特殊字符
      */
